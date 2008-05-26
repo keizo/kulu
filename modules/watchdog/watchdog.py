ecf548e7dd24fcc1239214cf7c7b1498bca10ee5
Keizo Gates <kzo@kzo.net>
2008-05-26 00:36:18 -1000

    worked a bit on getting watchdog working.  added the watchdog recent page, but didn't finish making the view nice. also renaming the project to 'kulu' -- hawaiian for drop

--- modules/watchdog/watchdog.py
+++ modules/watchdog/watchdog.py
@@ -10,9 +10,12 @@
 from web import form
 import web
 from drupy import *
+import loader
+inc = loader.import_('includes')
 
 urls = (
-    '/admin/logs','overview',
+    '/admin/logs','watchdog_overview',
+    '/admin/logs/recent', 'watchdog_recent',
 #    '/admin/logs/hits','hits',
 #    '/admin/logs/access-denied','access_denied',
 #    '/admin/logs/page-not-found','pagenotfound',
@@ -20,14 +23,34 @@ urls = (
 #    '/admin/logs/status','status'
     )
 
-class overview(page):
+class watchdog_overview(page):
     def GET(self):
         page = self.page
         form = form_overview()
-        print form.render()
-        print 'watchdog overview goes here'
+        content = form.render()
+        content += 'watchdog overview goes here'
+        web.render('generic.html')
+
+        
+class watchdog_recent(page):
+    def GET(self):
+        page = self.page
+        
+        iter_entries, page_nums = inc.pager.query('''SELECT w.wid, \
+        w.uid, w.severity, w.type, w.timestamp, w.message, w.link, \
+        u.name FROM watchdog w INNER JOIN users u \
+        ON w.uid = u.uid''', limit=100)
+        
+        content = ''
+        for entry in iter_entries:
+            content += str(entry)
+            
+        content += page_nums.render()
+        web.render('generic.html')
+        
         
 def drupy_cron():
+    """clean out old stuff in the watchdog table"""
     time1 = time() - variable_get('watchdog_clear', 604800)
     time2 = time() - 3600
     web.transact()
